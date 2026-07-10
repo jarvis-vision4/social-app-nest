06e2701452d101d2585169f6b793ce0563f8a768
Thurein Cloud <thureinh64@gmail.com>
2026-07-10 18:17:30 +0630

    Add User Module and configure env,versioning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://thurein:GjuDHR0B01ChIA6a@cluster0.xjxkvz1.mongodb.net/social_db?appName=Cluster0'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
